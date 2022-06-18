4d5e9d6b7b2e5640db8f51ea33b085b4c3cd6246
Kevin Chen <kc3585@nyu.edu>
2022-06-17 21:27:35 -0400

    add dynamic template

--- sender.py
+++ sender.py
@@ -1,6 +1,7 @@
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
 
+from distutils.command.install_egg_info import to_filename
 import os
 from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
@@ -8,11 +9,23 @@ from sendgrid.helpers.mail import Mail
 
 load_dotenv()
 
+TEMPLATE_ID = 'd-57dfbfc5e6b7413db633c56cce560a4b'
+
+# Original HTML Message
+# message = Mail(
+#     from_email='kevin@theheadstarter.com',
+#     to_emails='kevinchen3856@gmail.com',
+#     subject='Sending with Twilio SendGrid is Fun',
+#     html_content='<strong>and easy to do anywhere, even with Python</strong>')
+
+TO_EMAIL="kevinchen3856@gmail.com"
 message = Mail(
-    from_email='kevin@theheadstarter.com',
-    to_emails='kevinchen3856@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    from_email="kevin@theheadstarter.com",
+    to_emails=TO_EMAIL)
+message.dynamic_template_data = {
+    'first': 'SendGrid Development',
+}
+message.template_id = TEMPLATE_ID
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
